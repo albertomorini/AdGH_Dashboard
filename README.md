dbdda3ff69ad0cfc5a3420a15035bcf985e5d428
albertomorini <99morini@gmail.com>
2025-07-12 11:15:42 +0200

    total garbage react native

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AdGuard Home Dashboard
 
-Working in progress
+I started this project with React Native, after 4days of "do I really need to write CSS?" I gave up and adopt Ionic.
+
+In 2025 writing CSS or adding external libreries to just have a decent UI cannot be real... React Native cannot be usable, is total garbage for that.
+
--- app/myComponents/Dashboard.jsx
+++ app/myComponents/Dashboard.jsx
@@ -1,20 +1,30 @@
 
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import "../globalStyles/global.css";
+
+import { ThemedText } from "@/components/ThemedText";
+import { vars } from 'nativewind';
 import { useContext } from "react";
 import { Button } from "react-native";
-import GlobalStyle from "../globalStyles/global";
 import { mycontext } from "../index";
+const userTheme = vars({
+    '--color-values': '0 255 0',
+    '--color-rgb': 'rbg(0 0 255)'
+});
+
+
+
 const Dashboard = () => {
     const ctx = useContext(mycontext)
     return (
-        <ThemedView >
-            <ThemedView style={[GlobalStyle.navbar,GlobalStyle.inlineContainer]}>
+        <ThemedView  >
+            <ThemedView className="flex-1 items-center justify-center bg-white">
                 <ThemedText style={{marginBottom:"10px"}}>
                     Welcome back {ctx?.Username?.Username}!
                 </ThemedText>
 
                 <Button
+                    values={"flex-end"}
                     title="logout"
                     color="red"
 
@@ -26,6 +36,7 @@ const Dashboard = () => {
             </ThemedView>
 
         </ThemedView>
+     
     )
 }
 
